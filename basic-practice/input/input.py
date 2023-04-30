32bcecebf65ffc4c28a5be009f4736aee6719621
zhangxiaoyu-zidif <zxysjtu@gmail.com>
2023-04-30 09:46:19 +0800

    add some input function`

---
+++ basic-practice/input/input.py
@@ -0,0 +1,11 @@
+print("welcome")
+
+g = input("Guess the number: ")
+
+guess = int(g)
+
+if guess == 5:
+    print("You win!")
+else:
+    print("You lose!")
+print("Game over!")
---
+++ basic-practice/request/request.py
@@ -0,0 +1,9 @@
+import urllib.request
+
+page = urllib.request.urlopen("http://www.beans-r-us.biz/prices-loyalty.html")
+
+text = page.read().decode("utf8")
+
+price = text[234:238]
+
+print(price)
